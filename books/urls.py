e6cf7ad6c2b5873a3696c27a659d1a8a2f94c92e
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-10 13:22:55 +0600

    routes defined

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path("todo_detail/<int:todo_id>/", views.todo_detail, name="todo_detail"),
-    # path("todo_edit/<int:todo_id>/", views.todo_edit, name="todo_edit"),
-    # path("todo_delete/<int:todo_id>/", views.todo_delete, name="todo_delete"),
+    path("details/<str:book_id>/", views.details, name="details"),
+    path("bookmark/<str:book_id>/", views.bookmark, name="bookmark"),
+    # path("history/<int:book_id>/", views.todo_edit, name="history"),
+    # path("favorite/<int:book_id>/", views.todo_delete, name="favorite"),
     path("signup/", views.register, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
