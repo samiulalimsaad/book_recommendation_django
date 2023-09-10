9f9dafd3663e3232094e832a68a2ffba5525c061
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-10 13:22:46 +0600

    views added for index and bookmark

--- books/views.py
+++ books/views.py
@@ -1,10 +1,38 @@
-import requests
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_user
 from django.contrib.auth import logout as logout_user
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
+
+from books.helpers import search_books
+from books.models import Bookmark, History
+
+# @login_required(login_url="login/")
+# def index(req) -> HttpResponse:
+#     if req.user.is_authenticated:
+#         if req.method == "POST":
+#             query = req.POST["query"]
+#             books = search_books(query)
+
+#             return render(
+#                 req,
+#                 "index.html",
+#                 {"is_authenticated": req.user.is_authenticated, "books": books},
+#             )
+#         else:
+#             user = req.user
+#             return render(
+#                 req,
+#                 "index.html",
+#                 {
+#                     "is_authenticated": req.user.is_authenticated,
+#                 },
+#             )
+
+#     else:
+#         return render(req, "login.html", {"is_authenticated": req.user.is_authenticated})
 
 
 # @login_required(login_url="login/")
@@ -12,12 +40,15 @@ def index(req) -> HttpResponse:
     if req.user.is_authenticated:
         if req.method == "POST":
             query = req.POST["query"]
-            res = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={query}")
-            data = res.json()
+            books = search_books(query)
             return render(
                 req,
                 "index.html",
-                {"is_authenticated": True, "books": data["items"]},
+                {
+                    "is_authenticated": req.user.is_authenticated,
+                    "books": books,
+                    "query": query,
+                },
             )
         else:
             user = req.user
@@ -25,12 +56,14 @@ def index(req) -> HttpResponse:
                 req,
                 "index.html",
                 {
-                    "is_authenticated": True,
+                    "is_authenticated": req.user.is_authenticated,
                 },
             )
 
     else:
-        return render(req, "login.html", {"is_authenticated": False})
+        return render(
+            req, "login.html", {"is_authenticated": req.user.is_authenticated}
+        )
 
 
 def register(request) -> HttpResponse:
@@ -65,3 +98,20 @@ def login(request) -> HttpResponse:
 def logout(request) -> HttpResponse:
     logout_user(request)
     return redirect("login")
+
+
+@login_required(login_url="login/")
+def bookmark(req: HttpRequest, book_id) -> HttpResponse:
+    user = req.user
+    book = Bookmark.objects.create(user=user, book_id=book_id)
+    book.save()
+    return JsonResponse({"success": True})
+
+
+@login_required(login_url="login/")
+def details(req, book_id) -> HttpResponse:
+    user = req.user
+    book = History.objects.create(user=user, book_id=book_id)
+    book.save()
+    current_url = req.get_full_path()
+    return redirect(current_url)
