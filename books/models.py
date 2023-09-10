b477aecd5f7549a72bf83c727bcd8d402bfdc9a2
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-10 13:22:22 +0600

    Favorite,  Bookmark, History, Review models added

--- books/models.py
+++ books/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Favorite(TimeStampMixin):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    book_id = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} - favorite"
+
+
+class Bookmark(TimeStampMixin):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    book_id = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} - bookmark"
+
+
+class History(TimeStampMixin):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    book_id = models.CharField(max_length=50)
+    read_count = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user.username} - history"
+
+
+class Review(TimeStampMixin):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    book_id = models.CharField(max_length=50)
+    review_count = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.user.username} - review"
