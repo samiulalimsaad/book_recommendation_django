e188f0c48b6903539cb0fba870571119c0643a93
samiulalimsaad <samiulalimsaad@gmail.com>
2023-09-10 13:21:47 +0600

    service added for search books and book

---
+++ books/helpers.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def search_books(query):
+    res = requests.get(
+        f"https://www.googleapis.com/books/v1/volumes?q={query}&key=AIzaSyDz5HKDZj3UX-oKP1tRMfczCXzZFRdaDM8"
+    )
+    data = res.json()
+
+    return data["items"]
+
+
+def search_book(query):
+    res = requests.get(
+        f"https://www.googleapis.com/books/v1/volumes/{query}?key=AIzaSyDz5HKDZj3UX-oKP1tRMfczCXzZFRdaDM8"
+    )
+    data = res.json()
+
+    return data["items"]
